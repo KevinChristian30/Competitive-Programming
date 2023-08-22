829004dfe8c68e0de46e0af057df6f8b1da3f278
Kevin Christian <kdotchrist30@gmail.com>
2023-08-22 11:02:34 +0700

    Finished Smallest Range Covering Elements from K Lists

--- Basics/smallestRangeCoveringElementsFromKLists.cpp
+++ Basics/smallestRangeCoveringElementsFromKLists.cpp
@@ -1,9 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> smallestRange(vector<vector<int>> nums) {
-  return nums[0];
+pair<int, int> calculateRangeAndIncrement(vector<vector<int>> nums, int pointers[]) {
+  pair<int, int> range = { INT_MAX, INT_MIN };
+  int lowerBound;
+
+  for (int i = 0; i < nums.size(); i++) {
+    int current = nums[i][pointers[i]];
+    if (current < range.first) { 
+      range.first = current;
+      lowerBound = i;
+    }
+    if (current > range.second) range.second = current;
+  }
+
+  pointers[lowerBound]++;
+
+  return range;
 }
+
+vector<int> smallestRange(vector<vector<int>> nums) {
+  int pointers[nums.size()] = {0};
+
+  pair<int, int> temp = calculateRangeAndIncrement(nums, pointers);
+  vector<int> ans = { temp.first, temp.second };
+
+  while (true) {
+    bool isFinished = false;
+    for (int i = 0; i < nums.size(); i++) {
+      if (pointers[i] >= nums[i].size()) {
+        isFinished = true;
+        break;
+      }
+    }
+
+    if (isFinished) break;
+
+    pair<int, int> temp = calculateRangeAndIncrement(nums, pointers);
+    if (temp.second - temp.first < ans[1] - ans[0]) {
+      ans[0] = temp.first;
+      ans[1] = temp.second;
+    }
+  }
+
+  return ans;
+} 
 
 int main() {
   vector<vector<int>> nums = {
